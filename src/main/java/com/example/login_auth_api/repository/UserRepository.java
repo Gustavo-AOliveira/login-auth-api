277770a911776e58f7d34ba89e437be4a3fb93c5
Gustavo Aquino <gustavo.aquino.dev@gmail.com>
2025-03-12 21:48:44 -0300

    Add security to application

--- src/main/java/com/example/login_auth_api/repository/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.login_auth_api.repository;
 import com.example.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String login);
 }
